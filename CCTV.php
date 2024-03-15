1a4d3c5919e3ef97cf99d9a2bde4440e8c65ab09
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2024-03-15 14:12:09 +0800

    refactor: fix nav produk layout && add CCTV pages

---
+++ CCTV.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Rumah Pintar Teknologi & Inovatif</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta content="Rumah Pintar Teknologi & Inovatif" name="keywords" />
+    <meta content="Rumah Pintar Teknologi & Inovatif" name="description" />
+    <?php include('layout/css.php') ?>
+  </head>
+  <body>
+    <!-- Spinner Start -->
+    <?php include('layout/spinner.php') ?>
+    <!-- Spinner End -->
+
+   <?php include('layout/nav_content.php') ?>
+
+   
+
+    
+
+    <!-- Footer Start -->
+    <?php include ('layout/footer.php')?>
+    <!-- Footer End -->
+
+    <?php include('layout/script.php')?>
+   
+  </body>
+</html>
--- layout/nav_menu.php
+++ layout/nav_menu.php
@@ -1,5 +1,12 @@
 <a href="index.php" class="nav-item nav-link <?php if (basename($_SERVER['PHP_SELF']) == 'index.php') echo 'active'; ?>">Beranda</a>
-<a href="product.php" class="nav-item nav-link <?php if (basename($_SERVER['PHP_SELF']) == 'product.php') echo 'active'; ?>">Produk</a>
+<div class="nav-item dropdown">
+    <a href="#" class="nav-link dropdown-toggle <?= in_array(basename($_SERVER['PHP_SELF']), ['CCTV.php', 'social_media.php', 'landing_page.php']) ? 'active' : '' ?>" data-bs-toggle="dropdown">Produk</a>
+    <div class="dropdown-menu m-0">
+        <a href="CCTV.php" class="dropdown-item <?php if (basename($_SERVER['PHP_SELF']) == 'CCTV.php') echo 'active'; ?>">CCTV</a>
+        <a href="social_media.php" class="dropdown-item <?php if (basename($_SERVER['PHP_SELF']) == 'social_media.php') echo 'active'; ?>">Social Media</a>
+        <a href="landing_page.php" class="dropdown-item <?php if (basename($_SERVER['PHP_SELF']) == 'landing_page.php') echo 'active'; ?>">Landing Page</a>
+    </div>
+</div>
 <a href="about.php" class="nav-item nav-link <?php if (basename($_SERVER['PHP_SELF']) == 'about.php') echo 'active'; ?>">Tentang Kami</a>
 <a href="service.php" class="nav-item nav-link <?php if (basename($_SERVER['PHP_SELF']) == 'service.php') echo 'active'; ?>">Layanan</a>
 <a href="portfolio.php" class="nav-item nav-link <?php if (basename($_SERVER['PHP_SELF']) == 'portfolio.php') echo 'active'; ?>">Portofolio</a>
